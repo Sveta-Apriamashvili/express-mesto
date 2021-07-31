a4c4ddff9e1274a62d3536178002f323a01ae77c
Sveta Apriamashvili <s.v.apriamashvili@yandex.com>
2021-07-31 23:08:53 +1000

    13_WIP

---
+++ controllers/cards.js
@@ -0,0 +1,28 @@
+const Card = require('../model/card');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const createCard = (req, res) => {
+  console.log(req.user._id);
+  const { name, link } = req.body;
+  const owner = req.user._id;
+  return Card.create({ name, link, owner })
+    .then((card) => res.status(200).send(card))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+};
---
+++ controllers/users.js
@@ -0,0 +1,27 @@
+const User = require('../model/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const getUser = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  return User.create({ name, about, avatar })
+    .then((user) => res.status(200).send(user))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+};
